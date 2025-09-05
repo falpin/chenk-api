8626c20a6ffcd7c308bc6f3c126b981a9ffabba5
falbue <cyansair05@gmail.com>
2025-09-05 20:56:56 +0500

    Роуты для работы с ключами

--- routes/main.py
+++ routes/main.py
@@ -1,13 +1,15 @@
 from flask import Blueprint, jsonify, request, abort, g
 from functools import wraps
-from middleware import setup_middleware, key_role
+from middleware import setup_middleware, key_role, refresh_api_keys
 import io
 from utils import *
 from database import SQL_request
+import logger
+
+logger = logger.setup()
 
 api = Blueprint('api', __name__)
 
 @api.route('/', methods=['GET'])
-@key_role()
 def example():
     return jsonify({"message": "API Работает"}), 200
