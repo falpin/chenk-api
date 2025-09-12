f82bc60a2e28b01e10393bf0e77c150d7c45b783
falbue <cyansair05@gmail.com>
2025-09-12 20:00:13 +0500

    Перенос на FAS

--- app.py
+++ app.py
@@ -1,35 +1,3 @@
-from flask import Flask
-from dotenv import load_dotenv
-from extensions import cors
-from routes import *
-import os
-import logger
+from FlaskAPIServer import *
 
-from utils import *
-from config import *
-
-
-load_dotenv()
-logger = logger.setup()
-
-for var in config.required_env_vars:
-    if not os.getenv(var):
-        raise EnvironmentError(f"Переменная окружения {var} не задана в .env")
-
-def create_app():
-    app = Flask(__name__)
-    
-    cors.init_app(app)
-
-    app.register_blueprint(api)
-
-    app.config["SECRET_KEY"] = SECRET_KEY
-    setup_middleware(app)
-
-    return app
-
-app = create_app()
-
-if __name__ == '__main__':
-    logger.info("Сервер запущен")
-    app.run(port=5000, debug=config.DEBUG, host='0.0.0.0')
+server.start_server()
--- routes/timetable.py
+++
@@ -1,5 +0,0 @@
-from .main import *
-
-@api.route('/groups', methods=['GET'])
-def groups():
-    return jsonify({"message": "Все группы"}), 200
